6e702bb4f0dc65a160619fb17a57010a9421eb61
hoani <hoani.bryson@gmail.com>
2022-01-07 15:30:56 +1300

    JSON buffering implemented

--- src/jsonBuffer/jsonBuffer.h
+++ src/jsonBuffer/jsonBuffer.h
@@ -20,28 +20,47 @@ class JSONBuffer {
     JSONBuffer() = default;
 
     void update(Stream& stream){
-        if (_done || _index >= N) {
+        if (_done) {
             return;
         }
 
         while (stream.available() > 0) {
             byte b = stream.read();
-            _buffer[_index++] = b;
+            if (_depth > 0) {
+                _buffer[_index++] = b;
+            }
             if (b == '{') {
+                if (_depth == 0) {
+                    _buffer[_index++] = b;
+                }
                 _depth += 1;
             } else if (b == '}'){
                 if (_depth > 0) {
                     _depth--;
                     if (_depth == 0) {
+                        _buffer[_index++] = '\0';
                         _done = true;
                         return;
                     }
                 }
             }
+
+            // Reset if we are running out of buffer.
+            if ((_index + 1) == N) {
+                _index = 0;
+                _depth = 0;
+            }
         }
     }
 
     bool done() { return _done; }
+
+    String getString() {
+        if (_done) {
+            return String(_buffer);
+        }
+        return String("");
+    }
 
 
 
--- test/jsonBufferTest.cpp
+++ test/jsonBufferTest.cpp
@@ -73,27 +73,88 @@ TEST_CASE("Done", "[JSONBuffer]" )
 TEST_CASE("update", "[JSONBuffer]" ) 
 {
     SECTION("Reads Serial"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "{\"msg\":\"Hello\"}";
+        b.update(Serial);
+
+        REQUIRE(Serial.buffer.empty());
     }
 
     SECTION("Does not read additional bytes"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "{\"msg\":\"Hello\"}{\"msg2\":";
+        b.update(Serial);
+
+        REQUIRE(String(Serial.buffer.data()) == String("{\"msg2\":"));
     }
 }
 
 TEST_CASE("getString", "[JSONBuffer]" ) 
 {
     SECTION("empty when not done"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "{\"msg\":\"Hello\"";
+        b.update(Serial);
+
+        REQUIRE(b.getString() == String(""));
     }
 
     SECTION("correct contents when done"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "{\"msg\":\"Hello\"}";
+        b.update(Serial);
 
+        REQUIRE(b.getString() == String("{\"msg\":\"Hello\"}"));
     }
 
     SECTION("reject garbage bytes"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "\"Garbage\":12345}{\"msg\":\"Hello\"}";
+        b.update(Serial);
 
+        REQUIRE(b.getString() == String("{\"msg\":\"Hello\"}"));
     }
 
     SECTION("multibuffering"){
+        Serial.buffer = "{\"msg\":\"Hello\"}{\"name\":\"Tony\",\"age\":37}{\"cmds\":[{\"left\": 5},{\"stop\":0}]}";
+        std::vector<String> expectedBuffers = {
+            "{\"msg\":\"Hello\"}",
+            "{\"name\":\"Tony\",\"age\":37}",
+            "{\"cmds\":[{\"left\": 5},{\"stop\":0}]}",
+        };
+        for (const String& expected : expectedBuffers) {
+            JSONBuffer<128> b{};
+            b.update(Serial);
 
+            REQUIRE(b.done());
+            REQUIRE(b.getString() == expected);
+        }
+
+        JSONBuffer<128> b{};
+        b.update(Serial);
+
+        REQUIRE(b.done() == false);
+    }
+
+    SECTION("reject buffers which are too large"){
+        JSONBuffer<16> b{};
+        Serial.buffer = "{\"Too large\":true,\"Garbage\":12345}{\"msg\":\"Hello\"}";
+        b.update(Serial);
+
+        REQUIRE(b.getString() == String("{\"msg\":\"Hello\"}"));
+    }
+
+    SECTION("multiple update"){
+        JSONBuffer<128> b{};
+        Serial.buffer = "{\"msg\":";
+        b.update(Serial);
+        REQUIRE(b.done() == false);
+
+        Serial.buffer = "\"Hello\"}";
+        b.update(Serial);
+        REQUIRE(b.done());
+
+        REQUIRE(b.getString() == String("{\"msg\":\"Hello\"}"));
     }
 }
 
